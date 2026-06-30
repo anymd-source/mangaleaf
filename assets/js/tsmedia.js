24e14af7209ccaa504b229fbd9a600839225e8cc
anymd-source <anymoviess@proton.me>
2026-06-30 12:10:41 +0530

    Delete assets directory

--- assets/js/tsmedia.js
+++
@@ -1,9 +0,0 @@
-function tsMedia(x) {
-  if (x.matches) {
-    jQuery("#titlemove").appendTo(jQuery("#mobiletitle"));
-  } else {
-  	jQuery("#titlemove").prependTo(jQuery("#titledesktop"));
-  }	
-}
-var tsmmedia = matchMedia("(max-width: 800px)");
-tsmmedia.addListener(tsMedia);
